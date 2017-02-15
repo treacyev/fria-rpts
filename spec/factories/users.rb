6ba538e4051f61187b914eec851667f67596ce60
Gerard Montemayor <grbmontemayor@gmail.com>
2017-02-16 00:42:32 +0800

    Add User tests; Add first_name and last_name attributes to User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "foo#{n}@sample.com" }
+    first_name "John"
+    last_name "Doe"
+    password "password"
+    password_confirmation "password"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create' do
+
+    it 'is valid with valid attributes' do
+      expect(FactoryGirl.build(:user)).to be_valid
+    end 
+
+    it 'is invalid without a first name' do
+      expect(FactoryGirl.build(:user, first_name: nil)).to be_invalid
+    end
+
+    it 'is invalid without a last name' do
+      expect(FactoryGirl.build(:user, last_name: nil)).to be_invalid
+    end
+
+    it 'is invalid with a duplicate email' do
+      duplicate = FactoryGirl.create(:user, email: "duplicate@gmail.com")
+      expect(FactoryGirl.build(:user, email: "duplicate@gmail.com")).to be_invalid
+    end
+  end
 end
