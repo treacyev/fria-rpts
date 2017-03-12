f088722f4db5e19bc3ce3f6bae1cf7fdb3fb1930
Jerick Callado <jccallado@up.edu.ph>
2017-03-12 20:10:15 +0800

    validations complete

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,4 +1,14 @@
 class Proposal < ApplicationRecord
   validates :title, presence: true
+  validates :rdCost, presence: true, numericality: { only_float: true }
+  validates_numericality_of :rdCost, greater_than_or_equal_to: 0
+  validates :objectives, presence: true
+  validates :expectedOutputs, presence: true
+  validates :description, presence: true
+  validates :weeklyHours, presence: true
+  validates :endorsement, presence: true
+  validates :submitAgency, presence: true
+  validates :attachment, presence: true
+
   mount_uploader :attachment, AttachmentUploader
 end
