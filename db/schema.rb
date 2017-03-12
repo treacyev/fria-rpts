65fcc87aee00dd8697802fb5e3d8e970ee8b9e50
Jerick Callado <jccallado@up.edu.ph>
2017-03-12 20:31:39 +0800

    tests done

--- db/schema.rb
+++ db/schema.rb
@@ -35,25 +35,6 @@ ActiveRecord::Schema.define(version: 20170312090838) do
     t.datetime "updated_at",          null: false
   end
 
-  create_table "trial2s", force: :cascade do |t|
-    t.string   "tile2"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "trial3s", force: :cascade do |t|
-    t.string   "title3"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "trials", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Proposal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create' do
+    it 'is invalid without a title' do
+      expect(FactoryGirl.build(:proposal, title: nil)).to be_invalid
+    end
+    it 'is invalid without a principal researcher' do
+      expect(FactoryGirl.build(:proposal, principalResearcher: nil)).to be_invalid
+    end
+    it 'is invalid without objectives' do
+      expect(FactoryGirl.build(:proposal, objectives: nil)).to be_invalid
+    end
+    it 'is invalid without a expected outputs' do
+      expect(FactoryGirl.build(:proposal, expectedOutputs: nil)).to be_invalid
+    end
+    it 'is invalid without a Description of Work Done and Accomplishemnts' do
+      expect(FactoryGirl.build(:proposal, description: nil)).to be_invalid
+    end
+    it 'is invalid without Estimated Number of Hours Per Week Devoted to this Project' do
+      expect(FactoryGirl.build(:proposal, weeklyHours: nil)).to be_invalid
+    end
+    it 'is invalid without an answer in endorsement' do
+      expect(FactoryGirl.build(:proposal, endorsement: nil)).to be_invalid
+    end
+    it 'is invalid without an answer in Have you submitted this proposal to any other funding agency' do
+      expect(FactoryGirl.build(:proposal, submitAgency: nil)).to be_invalid
+    end
+    it 'is invalid without an attachment' do
+      expect(FactoryGirl.build(:proposal, attachment: nil)).to be_invalid
+    end
+  end
 end
