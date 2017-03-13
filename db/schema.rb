98b5011e180a2fa6a4b960aec0fcf493ac8c87c4
Jerick Callado <jccallado@up.edu.ph>
2017-03-14 00:41:19 +0800

    status of proposal included

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313162007) do
+ActiveRecord::Schema.define(version: 20170313163746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "announcement2s", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.boolean  "isDraft"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -56,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170313162007) do
     t.string   "attachment"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "status"
   end
 
   create_table "reviews", force: :cascade do |t|
