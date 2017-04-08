135c72e7bae1b7c8bb59852c09869cd37cd36e11
Gerard Montemayor <grbmontemayor@gmail.com>
2017-04-09 02:15:06 +0800

    Add icons, flash, semantic.js, main.js

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.message .close')
+    .on('click', function() {
+        $(this)
+        .closest('.message')
+        .transition('fade')
+        ;
+        console.log("hi");
+    })
+    ;
+});
