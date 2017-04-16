b4a2cb93354cb2519fe673fb1e321677ab8905c0
Gerard Montemayor <grbmontemayor@gmail.com>
2017-04-16 19:27:05 +0800

    Add sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_create :set_user_type
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable 
   # has_many :proposals
 
   validates :first_name, :last_name, :rank, :department, :contact_number, presence: true
+
+  def set_user_type
+    self.type ||= 'Researcher'
+  end
 end
