f281b2719fc11945675b871df64b7db009ae36e7
Jerick Callado <jccallado@up.edu.ph>
2017-04-16 20:37:40 +0800

    Proposal status changed to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416082501) do
+ActiveRecord::Schema.define(version: 20170416123653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,8 @@ ActiveRecord::Schema.define(version: 20170416082501) do
     t.string   "attachment"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-    t.string   "status"
     t.integer  "user_id"
+    t.integer  "status"
     t.index ["user_id"], name: "index_proposals_on_user_id", using: :btree
   end
 
