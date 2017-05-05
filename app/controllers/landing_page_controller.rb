20e39b3c8e83222fe3c79e2c9cde28b5d5ca761e
Gerard Montemayor <grbmontemayor@gmail.com>
2017-05-06 02:17:26 +0800

    Add proposals page for committee

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -6,6 +6,9 @@ class LandingPageController < ApplicationController
         if current_user && current_user.type == 'Researcher'
             @proposals = current_user.proposals.last(5).reverse
             render 'researcher_page/home'
+        elsif current_user && current_user.type != 'Admin'
+            @proposals = Proposal.last(5).reverse
+            render 'researcher_page/home'
         else
             @proposals = Proposal.where(status: 2).last(5).reverse
         end
