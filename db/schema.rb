c135cc5293285392cd0ca096a51a99c244bb8fea
Jerick Callado <jccallado@up.edu.ph>
2017-05-05 23:16:20 +0800

    migrations fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505135432) do
+ActiveRecord::Schema.define(version: 20170505151347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,12 +76,6 @@ ActiveRecord::Schema.define(version: 20170505135432) do
     t.string   "type"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "decision"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "proposals", "users"
